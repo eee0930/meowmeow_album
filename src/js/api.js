c33b0fd6e27c223b8073a10ad67072d2f389ddc9
lazyhysong <37135523+eee0930@users.noreply.github.com>
2023-03-22 17:15:00 +0900

    feat: category 구현

--- src/js/api.js
+++ src/js/api.js
@@ -1,27 +1,30 @@
 const FETCH_URL = "https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev";
 const IMG_PATH = "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-1ooef0cg8h3vq.s3.ap-northeast-2.amazonaws.com/public";
 
-// get root dirs api
-async function fetchRootDirs() {
-    const response = await fetch(FETCH_URL);
-    try {
-        const data = await response.json();
-        return data;
-    } catch(error) {
-        isLoading(false);
-        console.log(response.status, 'Unable to get the currency rate');
-    }
-}
-
 // get dirs or files by id api
 async function fetchDirOrFilesById(nodeId) {
-    const response = await fetch(`${FETCH_URL}/${nodeId}`);
     try {
-        const data = await response.json();
-        return data;
+        let response = '';
+        if(nodeId) {
+            response = await fetch(`${FETCH_URL}/${nodeId}`);
+        } else {
+            response = await fetch(FETCH_URL);
+        }
+        const data = response.json();
+        const status = response.status;
+        console.log(status);
+        if (status !== 200) {
+            setTimeout(() => {
+                isLoading(false);
+                console.log(status, 'API Error가 발생했다냥');
+            }, 2000);
+            return;
+        } else {
+            return data;
+        }
     } catch(error) {
         isLoading(false);
-        console.log(response.status, 'Unable to get the currency rate');
+        console.log(error, 'API Error가 발생했다냥');
     }
 }
 
--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,14 @@ const dirEle = document.querySelector(".dirEle");
 const fileEle = document.querySelector(".fileEle");
 const imageModal = document.querySelector("#imageModal");
 const loadingModal = document.querySelector("#loadingModal"); 
+
 const LOCALROOT = "localRootItems";
 const LOCALDIR = "localDirItems";
 const ROOT = "root";
+const BLOCK = "block";
+const NONE = "none";
 
+let nowNode = {};
 let categories = [];
 
 // div child 요소들 비우기
@@ -19,52 +23,62 @@ function removeAllEle(ele) {
     }
 };
 
-function movePrevDir(ele) {
-    setCategories(false, ele);
-    const nodeId = ele.id;
-    const name = ele.name;
-    if(name === ROOT) {
+function handleClickCategory(index) {
+    categories = categories.slice(0, index + 1);
+    const nodeId = categories[index].id;
+    const nodeName = categories[index].name;
+    nowNode = {
+        id: nodeId,
+        name: nodeName,
+    }
+    if(nodeId === -1) {
         getRootDirs();
     } else {
-        getDirOrFilesById(nodeId, name);
+        getDirOrFilesById();
     }
+    setCategories(nodeId);
 };
 
-// 카테고리 세팅
-function setCategories(isAdd, ele) {
+function markupCategories(category, index) {
+    const div = document.createElement("div");
+    div.innerHTML = category.name;
+    div.addEventListener("click", () => handleClickCategory(index));
+    breadcrumb.appendChild(div);
+}
+
+// 카테고리 세팅   
+function setCategories(nodeId) {
     removeAllEle(breadcrumb);
-    const eleId = ele.id;
-    if(isAdd) {
-        const cate = {
-            id: eleId,
-            name: ele.name,
-        };
-        categories.push(cate);
+    if(categories.length < 1) {
+        categories.push(nowNode);
     } else {
-        let cateIndex = 0;
+        let isIncluded = false;
+        let index = 0;
         for(let i = 0; i < categories.length; i++) {
-            if(categories[i].id === eleId) {
-                cateIndex = i;
+            const selected = categories[i];
+            if(selected.id === nodeId) {
+                isIncluded = true;
+                index = i;
                 break;
             }
         }
-        categories = categories.slice(0, cateIndex + 1);
+        if(isIncluded) {
+            categories = categories.slice(0, index + 1);
+        } else {
+            categories.push(nowNode);
+        }
     }
-    categories.map((cate, i) => {
-        const cateDiv = document.createElement("div");
-        const name = cate.name;
-        cateDiv.innerText = name;
-        cateDiv.addEventListener("click", () => movePrevDir(ele));
-        breadcrumb.appendChild(cateDiv);
-    })
+    categories.map((category, i) => {
+        markupCategories(category, i);
+    });
 }
 
 // 로딩 모달 띄우기
 function isLoading(isLoading) {
     if(isLoading) {
-        loadingModal.style.display = "block";
+        loadingModal.style.display = BLOCK;
     } else {
-        loadingModal.style.display = "none";
+        loadingModal.style.display = NONE;
     }
 }
 
@@ -73,36 +87,57 @@ function openImageViewer(filePath) {
     const loadingImg = imageModal.querySelector("#loadingImg");
     const dataImg = imageModal.querySelector("#dataImg");
     dataImg.src = fetchImageFile(filePath);
-    imageModal.style.display = "block";
+    imageModal.style.display = BLOCK;
     setTimeout(function() {
         imageModal.classList.remove("Loading");
         imageModal.classList.add("ImageViewer");
-        loadingImg.style.display = "none";
-        dataImg.style.display = "block";
+        loadingImg.style.display = NONE;
+        dataImg.style.display = BLOCK;
     }, 500);
 }
 // 모달 닫기
 function closeModal() {
     const loadingImg = imageModal.querySelector("#loadingImg");
     const dataImg = imageModal.querySelector("#dataImg");
-    loadingImg.style.display = "block";
-    dataImg.style.display = "none";
-    imageModal.style.display = "none";
+    loadingImg.style.display = BLOCK;
+    dataImg.style.display = NONE;
+    imageModal.style.display = NONE;
     imageModal.classList.remove("ImageViewer");
     imageModal.classList.add("Loading");
 }
 
+function handleClickPrevBtn() {
+    categories.pop();
+    const nodeId = categories[categories.length - 1].id;
+    const nodeName = categories[categories.length - 1].name;
+    nowNode = {
+        id : nodeId,
+        name: nodeName,
+    };
+    if(nodeId === -1) {
+        getRootDirs();
+    } else {
+        getDirOrFilesById();
+    }
+    setCategories(nodeId);
+}
+
 // markup prev btn
-function markupPrevBtn(parentId, parentName) {
+function markupPrevBtn() {
     const prev = prevBtn.cloneNode(true);
     prev.classList.remove("prevBtn");
-    prev.style.display = "block";
-    const ele = {
-        id: parentId,
-        name: parentName,
-    };
-    prev.addEventListener("click", () => movePrevDir(ele));
+    prev.style.display = BLOCK;
+    prev.addEventListener("click", () => handleClickPrevBtn());
     nodes.appendChild(prev);
+}
+
+function handleClickDir(id, name) {
+    nowNode = {
+        id,
+        name,
+    };
+    getDirOrFilesById();
+    setCategories(id);
 }
 // markup file or dir
 function markupFileOrDir(data) {
@@ -115,12 +150,26 @@ function markupFileOrDir(data) {
     } else {
         markupDiv = dirEle.cloneNode(true);
         markupDiv.classList.remove("dirEle");
-        markupDiv.addEventListener("click", () => getDirOrFilesById(id, name));
+        markupDiv.addEventListener("click", () => handleClickDir(id, name));
     }
-    markupDiv.style.display = "block";
+    markupDiv.style.display = BLOCK;
     const dataName = markupDiv.querySelector(".name");
     dataName.innerText = name;
     nodes.appendChild(markupDiv);
+}
+
+// get api fetch items
+async function getFetchItems(nodeId, localData) {
+    let datas = '';
+    if(nodeId) {
+        datas = await fetchDirOrFilesById(nodeId);
+        localData[nodeId] = datas;
+        localStorage.setItem(LOCALDIR, JSON.stringify(localData));
+    } else {
+        datas = await fetchDirOrFilesById();
+        localStorage.setItem(LOCALROOT, JSON.stringify(datas));
+    }
+    return datas;
 }
 
 // root dir 가져오기
@@ -128,63 +177,49 @@ async function getRootDirs() {
     removeAllEle(nodes);
     isLoading(true);
     app.dataset.id = ROOT;
-    const ele = {
-        id: null,
-        name: ROOT,
-    };
-    setCategories(true, ele);
     let localRoot = localStorage.getItem(LOCALROOT);
     let rootDatas = [];
     if(localRoot) {
-        rootDatas = localRoot;
+        rootDatas = JSON.parse(localRoot);
     } else {
-        rootDatas = await fetchRootDirs();
-        localStorage.setItem(LOCALROOT, JSON.stringify(rootDatas));
+        rootDatas = await getFetchItems();
     }
-    
     rootDatas.map((rootData) => {
         markupFileOrDir(rootData);
     });
     isLoading(false);
 }
 
-// children dir or files 가져오기
-async function getDirOrFilesById(nodeId, nodeName) {
+// nodeId로 dir or files 가져오기
+async function getDirOrFilesById() {
     removeAllEle(nodes);
     isLoading(true);
-    app.dataset.id = `${nodeId}`;
-    const ele = {
-        id: nodeId,
-        name: nodeName,
-    };
-    setCategories(true, ele);
+    app.dataset.id = `${nowNode.id}`;
     let localDir = localStorage.getItem(LOCALDIR);
     let dirDatas = [];
     let parcedLocalDir = {};
     if(localDir) {
         parcedLocalDir = JSON.parse(localDir);
-        if(parcedLocalDir[nodeId] !== undefined) {
-            dirDatas = parcedLocalDir[nodeId];
+        if(parcedLocalDir[nowNode.id] !== undefined) {
+            dirDatas = parcedLocalDir[nowNode.id];
         } else {
-            dirDatas = await fetchDirOrFilesById(nodeId);
-            parcedLocalDir[nodeId] = dirDatas;
-            localStorage.setItem(LOCALDIR, JSON.stringify(parcedLocalDir));
+            dirDatas = await getFetchItems(nowNode.id, parcedLocalDir);
         }
     } else {
-        dirDatas = await fetchDirOrFilesById(nodeId);
-        parcedLocalDir[nodeId] = dirDatas;
-        localStorage.setItem(LOCALDIR, JSON.stringify(parcedLocalDir));
+        dirDatas = await getFetchItems(nowNode.id, parcedLocalDir);
     }
-    const parentId = categories[categories.length - 2].id;
-    const parentName = categories[categories.length - 2].name; 
-    markupPrevBtn(parentId, parentName);
+    markupPrevBtn();
     dirDatas.map((dirData) => {
         markupFileOrDir(dirData);
     });
     isLoading(false);
 }
 
-
 if(app.dataset.id === ROOT) {
+    nowNode = {
+        id: -1,
+        name: ROOT,
+    };
     getRootDirs();
+    setCategories(nowNode.id);
 }
